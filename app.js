a74c4af91dc22ca4eaef301585b93b773dc47cd5
Matt Burton <mattwburton@gmail.com>
2021-11-02 14:37:09 +0000

    works needs tweaking

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const which = document.getElementById("which");
 const key = document.getElementById("key");
 const whichSmall = document.getElementById("which-small");
 const code = document.getElementById("code");
+const wrapper = document.getElementById("wrapper")
+//const location = document.getElementById("location");
+const welcome = document.getElementById("welcome")
 
 document.addEventListener("keypress", (e) => {
     console.log(e);
@@ -9,9 +12,14 @@ document.addEventListener("keypress", (e) => {
     console.log(e.charCode);
     console.log(e.key);
     console.log(e.code);
+    //console.log(e.location);
   
     which.innerHTML = `<p>${e.charCode}</p>`;
     key.innerHTML = `<p>${e.key}</p>`;
     whichSmall.innerHTML = `<p>${e.charCode}</p>`;
     code.innerHTML = `<p>${e.code}</p>`;
+    welcome.innerHTML =`<h1></h1>`;
+
+    //location.innerHTML = `<p>${e.location}</p>`;
+
 })
