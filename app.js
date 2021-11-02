e6dc5a29c87023d7fb76ae40f93c854d2f06ffbf
Matt Burton <mattwburton@gmail.com>
2021-11-02 11:10:28 +0000

    old code removed from app.js

--- app.js
+++ app.js
@@ -16,36 +16,3 @@ document.addEventListener("keypress", (e) => {
     keyCode.innerHTML = `<p>${e.keyCode}</p>`;
     
 })
-
-
-
-//     //let element = document.getElementById("which");
-//     //element.removeChild(paraWhich); 
-//     let paraWhich = document.createElement("p");
-//     let nodeWhich = document.createTextNode(e.charCode);
-//     paraWhich.appendChild(nodeWhich);
-//     let elementWhich = document.getElementById("which");
-//     elementWhich.appendChild(paraWhich);
-//     let paraKey = document.createElement("p");
-//     let nodeKey = document.createTextNode(e.key);
-//     paraKey.appendChild(nodeKey);
-//     let elementKey = document.getElementById("key");
-//     elementKey.appendChild(paraKey);
-//     let paraWhichSmall = document.createElement("p");
-//     let nodeWhichSmall = document.createTextNode(e.charCode);
-//     paraWhichSmall.appendChild(nodeWhichSmall);
-//     let elementWhichSmall = document.getElementById("which-small");
-//     elementWhichSmall.appendChild(paraWhichSmall);
-//     let paraCode = document.createElement("p");
-//     let nodeCode = document.createTextNode(e.code);
-//     paraCode.appendChild(nodeCode);
-//     let elementCode = document.getElementById("code");
-//     elementCode.appendChild(paraCode);
-// })
-
-//create new <p>, add text to <p>, append new <p> and text to named element
-// const para = document.createElement("p");
-// const node = document.createTextNode(e.charCode);
-// para.appendChild(node);
-// const element = document.getElementById("which");
-// element.appendChild(para);
